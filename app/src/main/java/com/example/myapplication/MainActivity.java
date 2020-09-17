da2a707c77eb98e7b2244a277145f9252c0a7c87
mohammed-A-yousif <71412422+mohammed-A-yousif@users.noreply.github.com>
2020-09-17 17:08:23 +0200

    item menu

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -47,8 +47,8 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.exit) {
+            finish();
         }
 
         return super.onOptionsItemSelected(item);
